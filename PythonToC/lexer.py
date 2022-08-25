56f7e9bfb8779cf2c55547ff74d7425b3b678bf3
Negin Shirvani <46245250+neginshirvani@users.noreply.github.com>
2022-08-25 05:03:59 -0400

    code

---
+++ PythonToC/lexer.py
@@ -0,0 +1,376 @@
+import ply.lex as lex
+
+
+class P2CLexer(object):
+    def __init__(self):
+        self.lexer = lex.lex(module=self)
+        print("Lexer build successfully")
+
+    # keyword constants
+    IF = 'IF'
+    TRUE = 'TRUE'
+    FALSE = 'FALSE'
+    OR = 'OR'
+    AND = 'AND'
+    ELSE = 'ELSE'
+    ELIF = 'ELIF'
+    FOR = 'FOR'
+    WHILE = 'WHILE'
+    RANGE = 'RANGE'
+    IN = 'IN'
+    NOT = 'NOT'
+    BREAK = 'BREAK'
+    CONTINUE = 'CONTINUE'
+
+    PRINT = 'PRINT'
+    STRING_LITERAL = 'STRING_LITERAL'
+
+    # relop constants
+    LT = 'LT'
+    LTE = 'LTE'
+    GT = 'GT'
+    GTE = 'GTE'
+    EQU = 'EQU'
+    NEQU = 'NEQU'
+
+    # arithmetic constants
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+    TIMES = 'TIMES'
+    DIV = 'DIV'
+    MOD = 'MOD'
+
+    # token constants
+    PLUS_EQUAL = 'PLUS_EQUAL'
+    MINUS_EQUAL = 'MINUS_EQUAL'
+    TIMES_EQUAL = 'TIMES_EQUAL'
+    DIV_EQUAL = 'DIV_EQUAL'
+
+    NUMBER = 'NUMBER'
+    ID = 'ID'
+    LOGIC = 'LOGIC'
+    RELOP = 'RELOP'
+    LPRAN = 'LPRAN'
+    RPRAN = 'RPRAN'
+    LBRACE = 'LBRACE'
+    RBRACE = 'RBRACE'
+    SEP = 'SEP'
+    EQ = 'EQ'
+    COLON = 'COLON'
+
+    ASSIGN = 'ASSIGN'
+    OPERATOR = 'OPERATOR'
+    KEYWORD = 'KEYWORD'
+
+    # reserved words
+    reserved = {
+        'if': IF,
+        'True': TRUE,
+        'False': FALSE,
+        'or': OR,
+        'and': AND,
+        'else': ELSE,
+        'elif': ELIF,
+        'for': FOR,
+        'while': WHILE,
+        'range': RANGE,
+        'in': IN,
+        'not': NOT,
+        'break': BREAK,
+        'continue': CONTINUE,
+        'print': PRINT,
+    }
+
+    # all the possible tokens
+    tokens = [ID, NUMBER, EQ,
+              GT, GTE, LT, LTE, EQU, NEQU,
+              AND, OR, NOT,
+              PLUS_EQUAL, TIMES_EQUAL, MINUS_EQUAL, DIV_EQUAL,
+              PLUS, MINUS, TIMES, DIV, MOD, STRING_LITERAL,
+              LPRAN, RPRAN, LBRACE, RBRACE, SEP, COLON, 'COMMENTS']
+    tokens += list(reserved.values())
+    # remove duplicate values
+    tokens = list(set(tokens))
+
+    # simple tokens regex definition
+    # arithmetic
+
+    def t_STRING_LITERAL(self, t):
+        r'\"([^\\\n]|(\\.))*?\"'
+        return t
+
+    def t_GTE(self, t):
+        r'(>=)'
+        return t
+
+    def t_EQU(self, t):
+        r'(==)'
+        return t
+
+    def t_NEQU(self, t):
+        r'(\!=)'
+        return t
+
+    # logic operators
+    def t_AND(self, t):
+        r'(&&)'
+        return t
+
+    def t_OR(self, t):
+        r'(\|\|)'
+        return t
+
+    def t_PLUS_EQUAL(self, t):
+        r'(\+=)'
+        return t
+
+    def t_DIV_EQUAL(self, t):
+        r'(/=)'
+        return t
+
+    def t_MINUS_EQUAL(self, t):
+        r'(\-=)'
+        return t
+
+    def t_TIMES_EQUAL(self, t):
+        r'(\*=)'
+        return t
+
+    def t_PLUS(self, t):
+        r'\+'
+        return t
+
+    def t_MINUS(self, t):
+        r'\-'
+        return t
+
+    def t_TIMES(self, t):
+        r'\*'
+        return t
+
+    def t_MOD(self, t):
+        r'%'
+        return t
+
+    # relop
+    def t_LTE(self, t):
+        r'(<=)'
+        return t
+
+    def t_LT(self, t):
+        r'<'
+        return t
+
+    def t_GT(self, t):
+        r'>'
+        return t
+
+    def t_NOT(self, t):
+        r'(\!)'
+        return t
+
+    # other
+    def t_LPRAN(self, t):
+        r'\('
+        return t
+
+    def t_RPRAN(self, t):
+        r'\)'
+        return t
+
+    def t_LBRACE(self, t):
+        r'\{'
+        return t
+
+    def t_RBRACE(self, t):
+        r'\}'
+        return t
+
+    def t_SEP(self, t):
+        r'\,'
+        return t
+
+    def t_EQ(self, t):
+        r'\='
+        return t
+
+    def t_DIV(self, t):
+        r"""(//)|/"""
+        t.value = '/'
+        return t
+
+    def t_COLON(self, t):
+        r':'
+        return t
+
+    def t_COMMENTS(self, t):
+        r'\#.*|""".*"""|".*"|\'.*\''
+
+    def t_ID(self, t):
+        r"""[a-zA-Z_][a-zA-Z_0-9]*"""
+        t.type = self.reserved.get(t.value, self.ID)
+        if t.type in [self.AND, self.OR, self.NOT]:
+            logical_symbols = {'and': '&&', 'or': '||', 'not': '!'}
+            t.value = logical_symbols.get(t.value)
+        elif t.type in [self.TRUE, self.FALSE]:
+            t.value = 1 if t.value == "True" else 0
+        return t
+
+    def t_NUMBER(self, t):
+        r"""[\+\-]?\d+(\.\d*)?"""
+        t.type = self.NUMBER
+        t.value = float(t.value)
+        return t
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = ' \t'
+
+    # Define a rule so we can track line numbers
+    @staticmethod
+    def t_newline(t):
+        r"""\n+"""
+        t.lexer.lineno += len(t.value)
+        # Error handling rule
+
+    @staticmethod
+    def t_error(t):
+        print("Illegal character '%s'" % t.value[0])
+
+    # Test it output
+    def test(self, input_data, answer_data):
+        self.lexer.input(input_data)
+        _tokens = []
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            _tokens.append(tok)
+
+        _error = False
+        for i in range(len(_tokens)):
+            try:
+                assert _tokens[i].type == answer_data[i][0] and _tokens[i].value == answer_data[i][1]
+                # print(_tokens[i])
+            except AssertionError:
+                print('***ERROR\n')
+                print(_tokens[i])
+                print(answer_data[i])
+                raise
+                _error = True
+
+        if not _error:
+            print("[LEXER] Test passed")
+
+
+if __name__ == '__main__':
+    lexer = P2CLexer()
+
+    # test 1
+    lexer.test("""
+    print("Hello world \n")
+    
+    # number and assignment tests
+        ifTrue = -3
+        b = +45
+        and = 1.2
+        andor = 1.2
+        d = 166.897
+        m = a
+        v3 = variable_Longer
+    
+    # relop tests
+    
+        a < b
+        other <= 3.3
+        var != other
+        5 > 6
+        89 >= 99
+        a == b
+    
+    # logic test
+        l1 and l2
+        l1 && l2
+        
+        wer or rew
+        wer || rew
+        not var
+        !var
+    
+    # keyword tests
+        
+        if ab >= 454:
+            a + b
+            for i in range(start, stop, step):
+                a%b
+                while condition != 0:
+        else False:
+            {
+                m = 34
+            }
+    
+    # operator tests
+        a + b
+        a - b
+        a * b
+        a / b
+        a//b
+        a%b
+        
+        m += n
+        m -= n
+        m *= n
+        m /= n
+
+    """, [
+        (lexer.PRINT, 'print'), (lexer.LPRAN, '('), (lexer.STRING_LITERAL, '"Hello world \n"'), (lexer.RPRAN, ')'),
+        (lexer.ID, 'ifTrue'), (lexer.EQ, '='), (lexer.MINUS, '-'), (lexer.NUMBER, 3),
+        (lexer.ID, 'b'), (lexer.EQ, '='), (lexer.PLUS, '+'), (lexer.NUMBER, 45),
+        (lexer.AND, '&&'), (lexer.EQ, '='), (lexer.NUMBER, 1.2),
+        (lexer.ID, 'andor'), (lexer.EQ, '='), (lexer.NUMBER, 1.2),
+        (lexer.ID, 'd'), (lexer.EQ, '='), (lexer.NUMBER, 166.897),
+        (lexer.ID, 'm'), (lexer.EQ, '='), (lexer.ID, 'a'),
+        (lexer.ID, 'v3'), (lexer.EQ, '='), (lexer.ID, 'variable_Longer'),
+        (lexer.ID, 'a'), (lexer.LT, '<'), (lexer.ID, 'b'),
+        (lexer.ID, 'other'), (lexer.LTE, '<='), (lexer.NUMBER, 3.3),
+        (lexer.ID, 'var'), (lexer.NEQU, '!='), (lexer.ID, 'other'),
+        (lexer.NUMBER, 5), (lexer.GT, '>'), (lexer.NUMBER, 6),
+        (lexer.NUMBER, 89), (lexer.GTE, '>='), (lexer.NUMBER, 99),
+        (lexer.ID, 'a'), (lexer.EQU, '=='), (lexer.ID, 'b'),
+        (lexer.ID, 'l1'), (lexer.AND, '&&'), (lexer.ID, 'l2'),
+        (lexer.ID, 'l1'), (lexer.AND, '&&'), (lexer.ID, 'l2'),
+        (lexer.ID, 'wer'), (lexer.OR, '||'), (lexer.ID, 'rew'),
+        (lexer.ID, 'wer'), (lexer.OR, '||'), (lexer.ID, 'rew'),
+        (lexer.NOT, '!'), (lexer.ID, 'var'),
+        (lexer.NOT, '!'), (lexer.ID, 'var'),
+        (lexer.IF, 'if'), (lexer.ID, 'ab'), (lexer.GTE, '>='), (lexer.NUMBER, 454), (lexer.COLON, ':'),
+        (lexer.ID, 'a'), (lexer.PLUS, '+'), (lexer.ID, 'b'),
+
+        (lexer.FOR, 'for'), (lexer.ID, 'i'), (lexer.IN, 'in'), (lexer.RANGE, 'range'),
+        (lexer.LPRAN, '('),
+        (lexer.ID, 'start'),
+        (lexer.SEP, ','),
+        (lexer.ID, 'stop'),
+        (lexer.SEP, ','),
+        (lexer.ID, 'step'),
+        (lexer.RPRAN, ')'),
+        (lexer.COLON, ':'),
+        (lexer.ID, 'a'), (lexer.MOD, '%'), (lexer.ID, 'b'),
+
+        (lexer.WHILE, 'while'), (lexer.ID, 'condition'), (lexer.NEQU, '!='), (lexer.NUMBER, 0), (lexer.COLON, ':'),
+        (lexer.ELSE, 'else'), (lexer.FALSE, 0), (lexer.COLON, ':'),
+        (lexer.LBRACE, '{'),
+        (lexer.ID, 'm'), (lexer.EQ, '='), (lexer.NUMBER, 34),
+        (lexer.RBRACE, '}'),
+
+        (lexer.ID, 'a'), (lexer.PLUS, '+'), (lexer.ID, 'b'),
+        (lexer.ID, 'a'), (lexer.MINUS, '-'), (lexer.ID, 'b'),
+        (lexer.ID, 'a'), (lexer.TIMES, '*'), (lexer.ID, 'b'),
+        (lexer.ID, 'a'), (lexer.DIV, '/'), (lexer.ID, 'b'),
+        (lexer.ID, 'a'), (lexer.DIV, '/'), (lexer.ID, 'b'),
+        (lexer.ID, 'a'), (lexer.MOD, '%'), (lexer.ID, 'b'),
+
+        (lexer.ID, 'm'), (lexer.PLUS_EQUAL, '+='), (lexer.ID, 'n'),
+        (lexer.ID, 'm'), (lexer.MINUS_EQUAL, '-='), (lexer.ID, 'n'),
+        (lexer.ID, 'm'), (lexer.TIMES_EQUAL, '*='), (lexer.ID, 'n'),
+        (lexer.ID, 'm'), (lexer.DIV_EQUAL, '/='), (lexer.ID, 'n'),
+    ])
---
+++ PythonToC/parser.py
@@ -0,0 +1,460 @@
+import json
+
+from lexer import P2CLexer as __
+import ply.yacc as yacc
+import re
+
+
+class P2CParser(object):
+    tokens = __.tokens
+    _ = __()
+    lexer = _.lexer
+    precedence = (
+        ('nonassoc', 'GTE', 'GT', 'LTE', 'LT', 'EQU', 'NEQU', 'AND', 'OR'),  # Nonassociative operators
+        ('left', 'PLUS', 'MINUS'),
+        ('left', 'TIMES', 'DIV', 'MOD'),
+        ('right', 'UMINUS', 'UPLUS'),
+
+    )
+
+    def __init__(self):
+        self.parser = yacc.yacc(module=self)
+        self.parse_tree = None
+        self.three_address_code = None
+
+        self.assign_symbols = ['=', '+=', '-=', '*=']
+        self.operation_symbols = ['+', '-', '*', '/', '&&', '||',
+                                  '==',
+                                  '>', '>=', '<', '<=', '!=', '%']
+        self.keywords = self._.reserved.keys()
+
+        self.symbol_table = {}
+
+        self.t_number = 0
+        self.l_number = 0
+
+    def get_temp(self):
+        self.t_number += 1
+        return "t%d" % self.t_number
+
+    def get_label(self):
+        self.l_number += 1
+        return "l%d" % self.l_number
+
+    # grammar rules start *********************
+
+    def p_statements(self, p):
+        """
+        statements : statements statement
+        | empty
+        """
+        if len(p) == 3:
+            if not p[1]:
+                p[1] = []
+            p[0] = p[1] + [p[2]]
+            self.parse_tree = p[0]
+
+    def p_statement(self, p):
+        """
+        statement : assignment
+                    | if
+                    | while
+                    | for
+                    | expr
+                    | print
+                    | CONTINUE
+                    | BREAK
+        """
+        p[0] = p[1]
+
+    def p_print(self, p):
+        """
+        print : PRINT LPRAN STRING_LITERAL print_args RPRAN
+        """
+        p[0] = tuple((p[1], p[3], p[4]))
+
+    def p_print_args(self, p):
+        """
+        print_args : expr
+                    | empty
+        """
+        p[0] = p[1]
+
+    def p_for(self, p):
+        """
+        for : FOR ID IN RANGE LPRAN params RPRAN COLON LBRACE statements RBRACE
+        """
+        p[0] = tuple((p[1], p[2], p[6], p[10]))
+
+    def p_params(self, p):
+        """
+        params : num_or_id
+            |   num_or_id SEP num_or_id
+            | num_or_id SEP num_or_id SEP num_or_id
+        """
+        if len(p) == 2:
+            p[0] = tuple((0, p[1], 1))
+        elif len(p) == 4:
+            p[0] = tuple((p[1], p[3], 1))
+        elif len(p) == 6:
+            p[0] = tuple((p[1], p[3], p[5]))
+
+    def p_while(self, p):
+        """
+        while : WHILE expr COLON LBRACE statements RBRACE
+        """
+        p[0] = tuple((p[1], p[2], p[5]))
+
+    def p_if(self, p):
+        """
+        if : IF expr COLON LBRACE statements RBRACE elif
+        """
+        p[0] = tuple((p[1], p[2], p[5], p[7]))
+
+    def p_elif(self, p):
+        """
+        elif : ELIF expr COLON LBRACE statements RBRACE elif
+        | else
+        """
+        if len(p) != 2:
+            p[0] = tuple((p[1], p[2], p[5], p[7]))
+        else:
+            p[0] = p[1]
+
+    def p_else(self, p):
+        """
+        else : ELSE COLON LBRACE statements RBRACE
+        | empty
+        """
+        if len(p) != 2:
+            p[0] = tuple((p[1], p[4]))
+
+    def p_assignment(self, p):
+        """
+        assignment : ID EQ expr
+                   | ID PLUS_EQUAL expr
+                   | ID MINUS_EQUAL expr
+                   | ID TIMES_EQUAL expr
+                   | ID DIV_EQUAL expr
+        """
+        p[0] = tuple((p[2], p[1], p[3]))
+
+    def p_exr_unary_minus(self, p):
+        """
+         expr : MINUS expr %prec UMINUS
+        """
+        p[0] = tuple((p[1], p[2]))
+
+    def p_exr_unary_plus(self, p):
+        """
+         expr : PLUS expr %prec UPLUS
+        """
+        p[0] = tuple((p[1], p[2]))
+
+    def p_expr_operator_relop(self, p):
+        """
+        expr : expr PLUS expr
+        | expr MINUS expr
+        | expr TIMES expr
+        | expr DIV expr
+        | expr MOD expr
+        | expr AND expr
+        | expr OR expr
+        | expr LT expr
+        | expr LTE expr
+        | expr GT expr
+        | expr GTE expr
+        | expr EQU expr
+        | expr NEQU expr
+        | ID
+        | NUMBER
+        | TRUE
+        | FALSE
+        """
+        if len(p) == 2:
+            p[0] = p[1]
+        elif len(p) == 4:
+            p[0] = tuple([p[2], p[1], p[3]])
+
+    def p_expr_pran(self, p):
+        """
+        expr : LPRAN expr RPRAN
+
+        """
+        p[0] = p[2]
+
+    def p_num_or_id(self, p):
+        """
+        num_or_id : NUMBER
+                | ID
+        """
+        p[0] = p[1]
+
+    def p_empty(self, p):
+        'empty :'
+        p[0] = None
+
+    # grammar rules end $$$$$$$$$$$$$$$$$$$$
+
+    def parse(self, input_data):
+        self.parser.parse(input_data, lexer=self.lexer)
+        return self.parse_tree
+
+    def get_tac(self, line):
+        if type(line) != tuple:
+            return "", line
+
+        if line[0] in self.assign_symbols:
+            return self.tac_assign(line)
+
+        elif line[0] in self.operation_symbols and len(line) == 3:
+            return self.tac_operator(line)
+        elif line[0] in self.operation_symbols and len(line) == 2:
+            return self.tac_operator_unary(line)
+        elif line[0] == 'if':
+            return self.tac_if_elif_else(line)
+        elif line[0] == 'while':
+            return self.tac_while(line)
+        elif line[0] == 'for':
+            return self.tac_for(line)
+        elif line[0] == 'print':
+            return self.tac_print(line)
+        else:
+            raise Exception("Invalid line: %s" % str(line))
+
+    def tac_print(self, line):
+        _str_format = line[1]
+        _arg = line[2]
+
+        if not _arg:
+            return f"printf({_str_format});\n", None
+        _code, root = self.get_tac(_arg)
+        return f"{_code}\nprintf({_str_format}, {root});\n", None
+
+    def tac_for(self, line):
+        for_var = line[1]
+        start, end, step = line[2]
+        statements = self.tac_program(line[3])
+
+        start_label = self.get_label()
+        end_label = self.get_label()
+
+        structure = ""
+
+        # define for variable if not defined before
+        if for_var not in self.symbol_table:
+            structure += f"float {for_var};\n"
+            self.symbol_table[for_var] = 'float'
+
+        # initialize the for variable
+        structure += f"{for_var} = {start};\n"
+        op = '>=' if step > 0 else '<='
+
+        structure += f"{start_label}:\n" \
+                     f"if ({for_var} {op} {end}) goto {end_label};\n" \
+                     f"{statements}\n" \
+                     f"{for_var} += {step};\n" \
+                     f"goto {start_label};\n" \
+                     f"{end_label}:;\n"
+        return structure, None
+
+    def tac_while(self, line):
+        condition = line[1]
+        statements = line[2]
+
+        condition_tac_str, condition_root = self.get_tac(condition)
+        statements_tac_str = self.tac_program(statements)
+
+        start_label = self.get_label()
+        end_label = self.get_label()
+
+        # the second time that we need to evaluate the condition
+        # no variable definitions will be needed
+        condition_tac_str_repeat = condition_tac_str.replace('float ', '')
+
+        structure = f"{start_label}: \n" \
+                    f"if (!{condition_root}) goto {end_label};\n" \
+                    f"{statements_tac_str}\n" \
+                    f"{condition_tac_str_repeat}\n" \
+                    f"goto {start_label};\n" \
+                    f"{end_label}:;\n"
+
+        return condition_tac_str + structure, None
+
+    def tac_if_elif_else(self, line):
+        data = []
+        _line = line
+        while _line:
+            keyword = _line[0]
+
+            if keyword == 'else':
+                data.append({
+                    'keyword': 'else',
+                    'statements': self.tac_program(_line[1])
+                })
+                break
+
+            if keyword == 'elif':
+                keyword = 'else if'
+            data.append({
+                'keyword': keyword,
+                'condition': self.get_tac(_line[1]),
+                'statements': self.tac_program(_line[2])
+            })
+
+            _line = _line[3]
+
+        # all the conditions first
+        conditions = ""
+        structure = ""
+        if_done_label = self.get_label()
+
+        for part in data:
+            keyword = part.get('keyword')
+            if keyword == 'else':
+                structure += f"{part.get('statements')}\n"
+                continue
+            _condition, condition_root = part.get('condition')
+            conditions += f"{_condition}\n"
+
+            statements_end = self.get_label()
+
+            structure += f"if (!{condition_root}) goto {statements_end};\n" \
+                         f"{part.get('statements')}\n" \
+                         f"goto {if_done_label};\n" \
+                         f"{statements_end}:;\n"
+
+        structure += f"{if_done_label}:;\n"
+        return conditions + structure, None
+
+    def tac_operator_unary(self, line):
+        a = line[1]
+        op = line[0]
+        a_tac_str, a_root = self.get_tac(a)
+        temp = self.get_temp()
+        return a_tac_str + f"float {temp} = {op}{a_root};\n", temp
+
+    def tac_operator(self, line):
+        a = line[1]
+        b = line[2]
+        op = line[0]
+
+        a_tac_str, a_root = self.get_tac(a)
+        b_tac_str, b_root = self.get_tac(b)
+
+        temp = self.get_temp()
+
+        _str = a_tac_str + b_tac_str
+        _str += f"float {temp} = {a_root} {op} {b_root};\n"
+
+        return _str, temp
+
+    def tac_assign(self, line):
+        lhs = line[1]
+        rhs = line[2]
+        op = line[0]
+
+        if lhs in self.symbol_table.keys():
+            # symbol defined before, no need to add type
+            _type = ''
+        else:
+            _type = 'float '
+            self.symbol_table[lhs] = 'float'
+
+        rhs_str, rhs_root = self.get_tac(rhs)
+        tac_str = f"{_type}{lhs} {op} {rhs_root};\n"
+        return rhs_str + tac_str, lhs
+
+    def tac_program(self, program):
+        if not program:
+            return "\n"
+        result = ""
+        for line in program:
+            _line, root = self.get_tac(line)
+            result += _line
+
+        return result
+
+    def generate_three_address_code(self):
+        body = self.tac_program(self.parse_tree)
+        c_tac_code = "#include <stdio.h>\nint main () {\n%s\nreturn 0;\n}" % body
+        c_tac_code = re.sub(' +', ' ', c_tac_code)
+        c_tac_code = re.sub('\n+', '\n', c_tac_code)
+        return c_tac_code
+
+    def test(self, input_data):
+        print(self.parse(input_data))
+
+
+def test_parse_tree_generation():
+    test_input = """
+        a = 10
+        b = 120
+
+        while a < b: {
+            if a == 20: { continue }
+            a += 1
+        }   
+
+        # a should be 120 by now
+
+        b *= 2  # b = 240
+        c = 0
+
+        for i in range(a, b, 2): {
+            c = i + a
+            if c > 200 : { break }
+            elif c == 200 : { continue }
+            else: {} 
+        }
+
+        result = 24 * ((a+b)-c/10)
+
+        """
+    test_output = """
+
+        [
+            ('=', 'a', 10.0), 
+            ('=', 'b', 120.0),
+            ('while', ('<', 'a', 'b'), 
+                [
+                    ('if', ('==', 'a', 20.0), ['continue'], None), 
+                    ('+=', 'a', 1.0)
+                ]), 
+
+             ('*=', 'b', 2.0), 
+             ('=', 'c', 0.0), 
+
+             ('for', 'i', ('a', 'b', 2.0), 
+                 [
+                    ('=', 'c', ('+', 'i', 'a')),
+                    ('if', ('>', 'c', 200.0), ['break'], ('elif', ('==', 'c', 200.0), ['continue'], ('else', None)))
+                  ]), 
+              ('=', 'result', ('*', 24.0, ('-', ('+', 'a', 'b'), ('/', 'c', 10.0))))
+        ]
+
+        """
+
+    # clean test_output
+    test_output = test_output.replace(' ', '').replace('\n', '')
+
+    _parser = P2CParser()
+    result = _parser.parse(test_input)
+
+    # clean result
+    result = str(result).replace(' ', '').replace('\n', '')
+    return test_output == result
+
+
+if __name__ == '__main__':
+    if not test_parse_tree_generation():
+        raise Exception("[PARSER] Parse tree test filed")
+
+    parser = P2CParser()
+    _input = open('program.py.txt')
+    test_input = _input.read()
+    parser.parse(test_input)
+    _out = open('program.c', 'w')
+    _out.write(parser.generate_three_address_code())
+    _input.close()
+    _out.close()
